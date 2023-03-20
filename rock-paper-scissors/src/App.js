893b2d1af173ac90dbe142618f396992e879f9ba
JiinArcade <jn_ll@naver.com>
2023-03-20 10:52:52 +0900

    ✅ update

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -2,11 +2,36 @@ import logo from './logo.svg';
 import Box from './component/Box';
 import './App.css'
 
+// 객체는 key : value 형태로 작성한다.
+// value로 여러개의 프로퍼티를 가질 수 있다.
+// 프로퍼티 : key : value 쌍
+const choics = {
+  scissors: {
+    name: "Scissors",
+    img: "https://t1.daumcdn.net/tistoryfile/fs12/3_tistory_2009_01_12_07_49_496a778f3d93a?original"
+  },
+  rock: {
+    name: "Rock",
+    img: "http://health.chosun.com/site/data/img_dir/2019/12/10/2019121001214_0.jpg"
+  },
+  paper: {
+    name: "Paper",
+    img: "https://src.hidoc.co.kr/image/lib/2020/3/27/1585295218513_0.jpg"
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <Box></Box>
-      <Box></Box>
+      <div class="Box-list">
+        <Box title="You"></Box>
+        <Box title="Computer"></Box>
+      </div>
+      <div className='btn-list'>
+        <button>가위</button>
+        <button>바위</button>
+        <button>보</button>
+      </div>
     </div>
   );
 }
--- rock-paper-scissors/src/component/Box.jsx
+++ rock-paper-scissors/src/component/Box.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Box = () => {
+const Box = (props) => {
   return (
     <div className='Box'>
-      <h1>User</h1>
+      <h1>{props.title}</h1>
       <h2>Rock</h2>
       <img src="http://health.chosun.com/site/data/img_dir/2019/12/10/2019121001214_0.jpg" alt="" />
       <h2>Win</h2>
