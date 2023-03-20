b441cefeaaf970bf5d6cd6ddb802f268feb40a7b
JiinArcade <jn_ll@naver.com>
2023-03-20 10:05:21 +0900

    ✅ update

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
+import Box from './component/Box';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-
+      <Box></Box>
+      <Box></Box>
     </div>
   );
 }
---
+++ rock-paper-scissors/src/component/Box.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Box = () => {
+  return (
+    <div className='Box'>
+      <h1>User</h1>
+      <h2>Rock</h2>
+      <img src="http://health.chosun.com/site/data/img_dir/2019/12/10/2019121001214_0.jpg" alt="" />
+      <h2>Win</h2>
+    </div>
+  )
+}
+
+export default Box
